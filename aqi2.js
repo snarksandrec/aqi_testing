7a976dedbc7817dd102c8e79964bfd52ab8039d9
snarksandrec <50960159+snarksandrec@users.noreply.github.com>
2020-10-08 17:38:52 -0700

    Update aqi2.js

--- aqi2.js
+++ aqi2.js
@@ -10,19 +10,19 @@ function getData() {
 }
 
 function updateHtml(data) {
-  let aqiPm25 = calcAQIpm25(data.pm25);
-  let aqiPm10 = calcAQIpm10(data.pm10);
+  let o_aqiPm25 = calcAQIpm25(data.pm25);
+  let o_aqiPm10 = calcAQIpm10(data.pm10);
 
   //update HTML
   document.getElementById("o_time").innerHTML = data.time;
-  document.getElementById("o_aqiPm25").innerHTML = aqiPm25;
-  document.getElementById("o_aqiPm10").innerHTML = aqiPm10;
+  document.getElementById("o_o_aqiPm25").innerHTML = o_aqiPm25;
+  document.getElementById("o_o_aqiPm10").innerHTML = o_aqiPm10;
   document.getElementById("o_pm25").innerHTML = "(PM2.5: " + data.pm25 + " µg/m³)";
   document.getElementById("o_pm10").innerHTML = "(PM10: " + data.pm10 + " µg/m³)";
 
   //set colors
-  colorsPm25 = getColor(aqiPm25);
-  colorsPm10 = getColor(aqiPm10);
+  colorsPm25 = getColor(o_aqiPm25);
+  colorsPm10 = getColor(o_aqiPm10);
   document.getElementById("o_containerPm25").style.background = colorsPm25.bg;
   document.getElementById("o_containerPm25").style.color = colorsPm25.text
   document.getElementById("o_containerPm10").style.background = colorsPm10.bg;
@@ -71,24 +71,24 @@ function calcAQIpm25(pm25) {
 	let aqi7 = 400;
 	let aqi8 = 500;
 
-	let aqipm25 = 0;
+	let o_aqiPm25 = 0;
 
 	if (pm25 >= pm1 && pm25 <= pm2) {
-		aqipm25 = ((aqi2 - aqi1) / (pm2 - pm1)) * (pm25 - pm1) + aqi1;
+		o_aqiPm25 = ((aqi2 - aqi1) / (pm2 - pm1)) * (pm25 - pm1) + aqi1;
 	} else if (pm25 >= pm2 && pm25 <= pm3) {
-		aqipm25 = ((aqi3 - aqi2) / (pm3 - pm2)) * (pm25 - pm2) + aqi2;
+		o_aqiPm25 = ((aqi3 - aqi2) / (pm3 - pm2)) * (pm25 - pm2) + aqi2;
 	} else if (pm25 >= pm3 && pm25 <= pm4) {
-		aqipm25 = ((aqi4 - aqi3) / (pm4 - pm3)) * (pm25 - pm3) + aqi3;
+		o_aqiPm25 = ((aqi4 - aqi3) / (pm4 - pm3)) * (pm25 - pm3) + aqi3;
 	} else if (pm25 >= pm4 && pm25 <= pm5) {
-		aqipm25 = ((aqi5 - aqi4) / (pm5 - pm4)) * (pm25 - pm4) + aqi4;
+		o_aqiPm25 = ((aqi5 - aqi4) / (pm5 - pm4)) * (pm25 - pm4) + aqi4;
 	} else if (pm25 >= pm5 && pm25 <= pm6) {
-		aqipm25 = ((aqi6 - aqi5) / (pm6 - pm5)) * (pm25 - pm5) + aqi5;
+		o_aqiPm25 = ((aqi6 - aqi5) / (pm6 - pm5)) * (pm25 - pm5) + aqi5;
 	} else if (pm25 >= pm6 && pm25 <= pm7) {
-		aqipm25 = ((aqi7 - aqi6) / (pm7 - pm6)) * (pm25 - pm6) + aqi6;
+		o_aqiPm25 = ((aqi7 - aqi6) / (pm7 - pm6)) * (pm25 - pm6) + aqi6;
 	} else if (pm25 >= pm7 && pm25 <= pm8) {
-		aqipm25 = ((aqi8 - aqi7) / (pm8 - pm7)) * (pm25 - pm7) + aqi7;
+		o_aqiPm25 = ((aqi8 - aqi7) / (pm8 - pm7)) * (pm25 - pm7) + aqi7;
 	}
-	return aqipm25.toFixed(0);
+	return o_aqiPm25.toFixed(0);
 }
 
 function calcAQIpm10(pm10) {
@@ -110,22 +110,22 @@ function calcAQIpm10(pm10) {
 	let aqi7 = 400;
 	let aqi8 = 500;
 
-	let aqipm10 = 0;
+	let o_aqiPm10 = 0;
 
 	if (pm10 >= pm1 && pm10 <= pm2) {
-		aqipm10 = ((aqi2 - aqi1) / (pm2 - pm1)) * (pm10 - pm1) + aqi1;
+		o_aqiPm10 = ((aqi2 - aqi1) / (pm2 - pm1)) * (pm10 - pm1) + aqi1;
 	} else if (pm10 >= pm2 && pm10 <= pm3) {
-		aqipm10 = ((aqi3 - aqi2) / (pm3 - pm2)) * (pm10 - pm2) + aqi2;
+		o_aqiPm10 = ((aqi3 - aqi2) / (pm3 - pm2)) * (pm10 - pm2) + aqi2;
 	} else if (pm10 >= pm3 && pm10 <= pm4) {
-		aqipm10 = ((aqi4 - aqi3) / (pm4 - pm3)) * (pm10 - pm3) + aqi3;
+		o_aqiPm10 = ((aqi4 - aqi3) / (pm4 - pm3)) * (pm10 - pm3) + aqi3;
 	} else if (pm10 >= pm4 && pm10 <= pm5) {
-		aqipm10 = ((aqi5 - aqi4) / (pm5 - pm4)) * (pm10 - pm4) + aqi4;
+		o_aqiPm10 = ((aqi5 - aqi4) / (pm5 - pm4)) * (pm10 - pm4) + aqi4;
 	} else if (pm10 >= pm5 && pm10 <= pm6) {
-		aqipm10 = ((aqi6 - aqi5) / (pm6 - pm5)) * (pm10 - pm5) + aqi5;
+		o_aqiPm10 = ((aqi6 - aqi5) / (pm6 - pm5)) * (pm10 - pm5) + aqi5;
 	} else if (pm10 >= pm6 && pm10 <= pm7) {
-		aqipm10 = ((aqi7 - aqi6) / (pm7 - pm6)) * (pm10 - pm6) + aqi6;
+		o_aqiPm10 = ((aqi7 - aqi6) / (pm7 - pm6)) * (pm10 - pm6) + aqi6;
 	} else if (pm10 >= pm7 && pm10 <= pm8) {
-		aqipm10 = ((aqi8 - aqi7) / (pm8 - pm7)) * (pm10 - pm7) + aqi7;
+		o_aqiPm10 = ((aqi8 - aqi7) / (pm8 - pm7)) * (pm10 - pm7) + aqi7;
 	}
-	return aqipm10.toFixed(0);
+	return o_aqiPm10.toFixed(0);
 }
