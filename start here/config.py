86dd9da5c118920dce1154d97237cc50157b8774
snarksandrec <50960159+snarksandrec@users.noreply.github.com>
2020-10-09 07:15:48 -0700

    Update config.py

--- start here/config.py
+++ start here/config.py
@@ -1,2 +1,2 @@
-github_token = "85291e0ab5c5406aa786f23c6ba69f23c7ba6fc5"
-iftt_key = "OSN9K3ugXH_e4Wu0IaGZZ"
+github_token = "TOKEN" #replace with token: https://github.com/settings/tokens
+iftt_key = "WEBHOOKS_KEY" #replace with key found in "Documentation" page of IFTT Webhooks section
